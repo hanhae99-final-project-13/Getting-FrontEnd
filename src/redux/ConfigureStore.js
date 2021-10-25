501cd8402c023870a3381c6a520e7dc613aa7d83
geemhunsu <sksguss2@gmail.com>
2021-10-26 01:06:21 +0900

    프로젝트 생성

---
+++ src/redux/ConfigureStore.js
@@ -0,0 +1,32 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { createBrowserHistory } from 'history';
+import { connectRouter } from 'connected-react-router';
+
+export const history = createBrowserHistory();
+
+const rootReducer = combineReducers({
+  
+  router: connectRouter(history),
+});
+
+const middlewares = [thunk.withExtraArgument({ history: history })];
+
+const env = process.env.NODE_ENV;
+
+if (env === 'development') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+
+let store = initialStore => createStore(rootReducer, enhancer);
+//테스트중입니다.
+
+export default store();
