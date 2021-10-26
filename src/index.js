a9ad15d0bf66f67bb40fcedf8f25724ab9038372
geemhunsu <sksguss2@gmail.com>
2021-10-26 12:18:39 +0900

    리액트 앱  초기설정

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './shared/App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore'
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.render(  
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/pages/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Grid } from '../elements';
+
+const Main = () => {
+  console.log('sdflkjsdf');
+  return (
+    <React.Fragment>
+      <Grid widht='500px' height='500px' bg='blue'>
+        sodyd
+      </Grid>
+      메인페이지입니다
+      <Grid></Grid>
+    </React.Fragment>
+  );
+};
+
+export default Main;
