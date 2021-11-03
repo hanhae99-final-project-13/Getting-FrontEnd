87866a55ca47be2529da2c4f050b47ceda09d275
JiHoon <skyy630@naver.com>
2021-11-03 18:40:57 +0900

    [feat] 알림 count 조건에 따른 뷰 구현

--- src/pages/Alarm.js
+++ src/pages/Alarm.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../components/Header';
 import { Grid, Text, Image } from '../elements/index';
 const Alarm = () => {
+  const alarmCount = useSelector(
+    (state) => state.user.user.userInfo.alarmCount,
+  );
+  console.log(alarmCount);
   const [deleteModal, setDeleteModal] = React.useState(false);
   const delModaltoggle = () => {
     setDeleteModal(!deleteModal);
@@ -25,7 +30,7 @@ const Alarm = () => {
             justifyContent='space-between'
             alignItems='center'
           >
-            <Text size='12px'>알림 {'1'}개</Text>
+            <Text size='12px'>알림 {alarmCount}개</Text>
             <button
               style={{
                 all: 'unset',
@@ -39,52 +44,69 @@ const Alarm = () => {
             </button>
           </Grid>
 
-          {/* 삼항연산자 넣을 곳 알람.length === 0 일때 ?알람없음 이미지 화면 : 아닐때 알람 리스트화면  */}
-          <Grid
-            bg='white'
-            height='60px'
-            padding='10px 5px'
-            borderRadius='15px'
-            width='auto'
-            display='flex'
-            alignItems='center'
-            margin='15px 0'
-            boxShadow='4px 4px 10px 0px rgba(0, 0, 0, 0.1)'
-          >
-            <Image margin='0 10px' size='50' />
-            <Grid display='flex' flexDirection='column' boxSizing='border-box'>
-              <Grid fontSize='12px' color='darkgrey'>
-                {'30분'} 전
+          {alarmCount === 0 ? (
+            <>
+              <Grid display='flex' justifyContent='center' alignItems='center'>
+                <Grid
+                  width='260px'
+                  height='260px'
+                  margin='70px 0 30px 0'
+                  bg='white'
+                  borderRadius='200px'
+                  color='black'
+                  boxShadow='4px 4px 20px rgba(0, 0, 0, 0.1)'
+                >
+                  <img
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      borderRadius: '200px',
+                      objectFit: 'cover',
+                    }}
+                    src='https://w.namu.la/s/4c9edd1a4c1cf019e364ad3b684c8e6537f834527ee6fb94dd9853b506a7163d1a0150c52864ef19d322cf5fa17bd5b1dd7e7db063889d4a832cb4c8784712fdaa65557a38e9eac3afa50a862472f273df9b2906cfbc0f84d324476519b424d7'
+                  />
+                </Grid>
               </Grid>
-              <Grid>{'닉네임'}님, 새로운 반려친구가 뭐시깽이~</Grid>
-              <Grid fontSize='12px' color='darkgrey'>
-                {'랜덤메세지 뭐시깽이'}
+              <Grid display='flex' justifyContent='center' margin='10px 0'>
+                <Text size='20px'>
+                  <span style={{ fontWeight: '800', fontSize: '20px' }}>
+                    최근 알림
+                  </span>
+                  이 없습니다!
+                </Text>
+              </Grid>
+            </>
+          ) : (
+            <Grid
+              bg='white'
+              height='60px'
+              padding='10px 5px'
+              borderRadius='15px'
+              width='auto'
+              display='flex'
+              alignItems='center'
+              margin='15px 0'
+              boxShadow='4px 4px 10px 0px rgba(0, 0, 0, 0.1)'
+            >
+              <Image margin='0 10px' size='50' />
+              <Grid
+                display='flex'
+                flexDirection='column'
+                boxSizing='border-box'
+              >
+                <Grid fontSize='12px' color='darkgrey'>
+                  {'30분'} 전
+                </Grid>
+                <Grid>{'닉네임'}님, 새로운 반려친구가 뭐시깽이~</Grid>
+                <Grid fontSize='12px' color='darkgrey'>
+                  {'랜덤메세지 뭐시깽이'}
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
+          )}
         </Grid>
 
         {/* 알람없을때 화면 */}
-        <Grid display='flex' justifyContent='center' alignItems='center'>
-          <Grid
-            width='260px'
-            height='260px'
-            bg='white'
-            borderRadius='200px'
-            color='black'
-            boxShadow='4px 4px 20px rgba(0, 0, 0, 0.1)'
-          >
-            알림 없을 경우 이미지
-          </Grid>
-        </Grid>
-        <Grid display='flex' justifyContent='center' margin='10px 0'>
-          <Text size='20px'>
-            <span style={{ fontWeight: '800', fontSize: '20px' }}>
-              최근 알림
-            </span>
-            이 없습니다!
-          </Text>
-        </Grid>
 
         {/* 전체삭제 모달 */}
         {deleteModal ? (
