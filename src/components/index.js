8b0244686efdc6f11380fcdd3341ba8b54199ea4
geemhunsu <sksguss2@gmail.com>
2021-11-22 23:55:40 +0900

    (김현수) PULL

--- src/components/index.js
+++ src/components/index.js
@@ -41,6 +41,7 @@ export {
   Logo,
   Post,
   Slider,
+  SliderBox,
   Timer,
   Upload,
 };
--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Grid } from '../elements';
+import { Grid, Text } from '../elements';
 import {
   MypageUserInfo,
   MypageWishedList,
@@ -13,7 +13,7 @@ import { BackButton, Header, Logo } from '../components';
 import { history } from '../redux/configureStore';
 import { postActions } from '../redux/modules/post';
 import { applyActions } from '../redux/modules/apply';
-import { ErrorAlert } from '../shared/Alerts';
+import { ErrorAlert, WarningAlert } from '../shared/Alerts';
 import WebSocket from './WebSocket';
 const Mypage = () => {
   const dispatch = useDispatch();
@@ -65,8 +65,29 @@ const Mypage = () => {
     history.goBack();
   }
 
+  const editButton = () => {
+    WarningAlert('서비스 준비중입니다');
+  };
+
   return (
-    <Grid maxWidth='414px' margin='0 auto'>
+    <Grid
+      position='relative'
+      maxWidth='414px'
+      margin='0 auto'
+      padding='63px 0 0 0'
+    >
+      <BackButton position='absolute' left='36px' top='21px' />
+      <Text
+        position='absolute'
+        right='36px'
+        top='21px'
+        margin='0'
+        size='12px'
+        weight='800'
+        _onClick={editButton}
+      >
+        수정하기
+      </Text>
       <WebSocket />
       <Grid margin='107px 0 0 0' width='auto'>
         <MypageUserInfo />
